258a3e9d6dffa6868f66082677f4ff787192450e
akubaik1 <87461412+akubaik1@users.noreply.github.com>
2022-03-04 15:19:53 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+function hbd() {
+  document.getElementById("gambar").src = "https://cdn.pixabay.com/photo/2016/11/18/15/46/birthday-1835443_960_720.jpg";
+  document.getElementById("tulisan").innerHTML = "Dari LetsGoy"
+}
